561fd90f1abefaf1a61dc409e7a97b66b617ae33
PRATYAY MUSTAFI <pratyaymustafi@outlook.com>
2026-02-23 23:42:45 +0530

    netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
-import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
--- dist/server/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/node_ekdSZlWr.mjs';
-export { renderers } from '../renderers.mjs';
