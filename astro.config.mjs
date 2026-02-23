e1101909a08ae1df9c63f6e01da88aab4718b9cf
PRATYAY MUSTAFI <pratyaymustafi@outlook.com>
2026-02-23 23:14:29 +0530

    migrate to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
   integrations: [react()],
 
   vite: {
